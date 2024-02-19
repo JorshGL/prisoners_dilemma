a96b0ab1f0a265c84e2ce45b11af00a0cd6c3151
sahv <sergio_and.herrera@uao.edu.co>
2024-02-18 19:24:31 -0500

    prueba

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,32 +1,34 @@
 <template>
-    <div class="bg-zinc-900 text-white h-full">
-        <NavBar/>
-        <div class="grid grid-cols-3 grid-rows-3 w-full h-fit p-10 space-x-5">
-            <div class="row-span-3">
-                <Controls/>
-            </div>
-            <div class="row-span-2 col-span-2">
-                <Characters/>
-            </div>
-            <div class="col-span-2">
-                <Results/>
-            </div>
-        </div>
+  <div class="bg-zinc-900 text-white h-full">
+    <NavBar />
+    <div class="grid grid-cols-3 grid-rows-3 w-full h-fit p-10 space-x-5">
+      <div class="row-span-3">
+        <Controls />
+      </div>
+      <div class="row-span-2 col-span-2">
+        <Characters />
+      </div>
+      <div class="col-span-2">
+        <Results />
+      </div>
+      <div>
+        <button>Hola</button>
+      </div>
     </div>
+  </div>
 </template>
 
-
 <script>
-import NavBar from '@/components/NavBar.vue';
-import Controls from '@/components/Home/Controls.vue';
-import Characters from '@/components/Home/Characters.vue';
-import Results from '@/components/Home/Results.vue';
+import NavBar from "@/components/NavBar.vue";
+import Controls from "@/components/Home/Controls.vue";
+import Characters from "@/components/Home/Characters.vue";
+import Results from "@/components/Home/Results.vue";
 export default {
-    components: {
+  components: {
     NavBar,
     Controls,
     Characters,
-    Results
-}
-}
+    Results,
+  },
+};
 </script>
