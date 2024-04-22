6b059beb6b81542ed44742e1457f7a2316d64af3
sergio <sergioandresherreravallejo@gmail.com>
2024-04-21 22:07:03 -0500

    New Characters implementation NOT FINAL DESIGN

--- src/components/Home/Characters.vue
+++ src/components/Home/Characters.vue
@@ -8,25 +8,24 @@
       class="z-10 grid auto-cols-min justify-center items-center gap-y-5 gap-x-24 p-4 text-center"
     >
       <div class="flex items-center justify-center">
-        <strong> {{ lang.playerATitle }}</strong>
+        <strong>{{ lang.playerATitle }}</strong>
       </div>
       <div
-        class="flex flex-col row-start-2 bg-zinc-900 h-60 w-48 rounded-xl items-center p-2 justify-center overflow-hidden"
+        class="flex flex-col row-start-2 bg-zinc-900 p-8 h-auto w-48 rounded-xl items-center justify-center overflow-hidden"
       >
         <img
-          class="hover:scale-125 transition duration-300 ease-in-out"
+          class="hover:scale-125 transition duration-400 ease-in-out animate-pulse"
           :src="data.playerA.sprite"
         />
       </div>
-
       <div class="flex items-center justify-center">
         <strong>{{ lang.playerBTitle }}</strong>
       </div>
       <div
-        class="flex flex-col row-start-2 bg-zinc-900 h-60 w-48 rounded-xl items-center p-2 justify-center overflow-hidden"
+        class="flex flex-col row-start-2 bg-zinc-900 h-auto w-48 rounded-xl items-center p-8 justify-center overflow-hidden"
       >
         <img
-          class="hover:scale-125 transition duration-300 ease-in-out"
+          class="hover:scale-125 transition duration-400 ease-in-out animate-pulse"
           :src="data.playerB.sprite"
         />
       </div>
